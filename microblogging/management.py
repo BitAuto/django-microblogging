7c7201174e0dee9f8c565a4aa6e423b5ef0ef265
Pascal Hartig <phartig@rdrei.net>
2012-01-20 14:09:33 +0100

    Updated to new django-notification API
    create_notice_type was removed in favor of a create() class method. The new
    version of django-notification is required in order to work with Django >= 1.4.

--- microblogging/management.py
+++ microblogging/management.py
@@ -2,11 +2,11 @@ from django.db.models import get_models, signals
 from django.utils.translation import ugettext_noop as _
 
 try:
-    from notification import models as notification
+    from notification.models import NotificationType
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("tweet_follow", _("New Tweet Follower"), _("someone has started following your tweets"))
-        notification.create_notice_type("tweet_reply_received", _("New Tweet Reply"), _("someone sent a tweet reply to you"))
+        NotificationType.create("tweet_follow", _("New Tweet Follower"), _("someone has started following your tweets"))
+        NotificationType.create("tweet_reply_received", _("New Tweet Reply"), _("someone sent a tweet reply to you"))
     
     signals.post_syncdb.connect(create_notice_types, sender=notification)
 except ImportError:
